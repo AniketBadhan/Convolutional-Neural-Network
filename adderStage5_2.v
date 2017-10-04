6b4d85d1f8adae44a69d52a6479918146cd593db
AniketBadhan <32457941+AniketBadhan@users.noreply.github.com>
2017-10-04 17:23:06 -0400

    Update adderStage5_2.v

--- adderStage5_2.v
+++ adderStage5_2.v
@@ -6,12 +6,12 @@
 `timescale 1ns / 1ps
 
 module adderStage5_2(
-    input [20:0] input1,
-    input [20:0] input2,
-    output reg [21:0] output1,
+    	input [20:0] input1,
+    	input [20:0] input2,
+    	output reg [21:0] output1,
 	input enable,
-    input clk,
-	 output reg done
+    	input clk,
+	output reg done
     );
 	
 	always @ (posedge clk) begin
