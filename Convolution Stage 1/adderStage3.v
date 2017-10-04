652a41bc14d110286428d3a9bdc784147b521a88
AniketBadhan <32457941+AniketBadhan@users.noreply.github.com>
2017-10-03 23:48:37 -0400

    Update adderStage3.v

--- Convolution Stage 1/adderStage3.v
+++ Convolution Stage 1/adderStage3.v
@@ -6,15 +6,20 @@
 `timescale 1ns / 1ps
 
 module adderStage3(
-    input [4:0] input1,
-    input [4:0] input2,
-    output reg [5:0] output1,
-    input clk
-    //input adderEnable
+    	input [4:0] input1,
+    	input [4:0] input2,
+    	output reg [5:0] output1,
+	input enable,
+    	input clk
     );
 
 	always @ (posedge clk) begin
-		output1 <= input1 + input2;	
+		if(enable) begin
+			output1 <= input1 + input2;	
+		end
+		else begin
+			output1 <= 0;	
+		end
 	end
 	
 endmodule
