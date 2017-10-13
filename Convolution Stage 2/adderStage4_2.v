24f1abbcd77b8558c620e8e88ef6889d451f4bb5
Aniket Badhan <aniketbadhan@umbc.edu>
2017-10-13 01:40:13 -0400

    Update adderStage4_2.v

--- Convolution Stage 2/adderStage4_2.v
+++ Convolution Stage 2/adderStage4_2.v
@@ -6,12 +6,12 @@
 `timescale 1ns / 1ps
 
 module adderStage4_2(
-    input [18:0] input1,
-    input [18:0] input2,
+    	input [18:0] input1,
+    	input [18:0] input2,
 	input [18:0] input3,
-    output reg [20:0] output1,
+    	output reg [20:0] output1,
 	input enable,
-    input clk
+    	input clk
     );
 	
 	always @ (posedge clk) begin
